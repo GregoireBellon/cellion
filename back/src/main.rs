03e6c7abc4b0301b27cf3389f39182a2f459b33d
Grégoire Bellon <gregoire.bellon@outlook.fr>
2024-05-21 23:02:16 +0200

    feat: lighten build with multistage and embedded migrations

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,7 +1,13 @@
+use std::error::Error;
+
 use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
-use diesel::{r2d2, SqliteConnection};
+use diesel::{r2d2, sqlite::Sqlite, SqliteConnection};
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use log::{error, info};
 mod api;
 mod models;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 type DbPool = r2d2::Pool<r2d2::ConnectionManager<SqliteConnection>>;
 
@@ -10,6 +16,13 @@ async fn hello() -> impl Responder {
     return HttpResponse::Ok().body("healthy");
 }
 
+fn run_migrations(
+    connection: &mut impl MigrationHarness<Sqlite>,
+) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    connection.run_pending_migrations(MIGRATIONS)?;
+
+    Ok(())
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenvy::dotenv().ok();
@@ -21,6 +34,12 @@ async fn main() -> std::io::Result<()> {
     let pool = r2d2::Pool::builder()
         .build(manager)
         .expect("database URL should be valid path to SQLite DB file");
+
+    run_migrations(&mut pool.get().unwrap())
+        .inspect(|_| info!("Migrations ran successfully"))
+        .inspect_err(|e| error!("{}", e))
+        .expect("The migrations failed");
+
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
